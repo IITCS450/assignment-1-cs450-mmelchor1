61bcb54f915228f90ecc82bb3b592e9e14fbd678
mmelchor1 <mmelchor1@hawk.iit.edu>
2026-01-27 13:01:33 -0500

    resetting the correct paths to retrieve the fields needed with corresponded error handling

--- src/procinfo.c
+++ src/procinfo.c
@@ -6,28 +6,30 @@ static void usage(const char *a){fprintf(stderr,"Usage: %s <pid>\n",a); exit(1);
 static int isnum(const char*s){for(;*s;s++) if(!isdigit(*s)) return 0; return 1;}
 int main(int c,char**v){
  if(c!=2||!isnum(v[1])) usage(v[0]);
- char path[256];
- char line[256];
- FILE *fp;
- //Path to the status file
- snprintf(path, sizeof(path), "/proc/%s", v[1]);
+ char path[256]; // path buffer
+ FILE *fp; // file pointer
 
- fp = fopen(path, "r");
+ //Path to the status file
+ snprintf(path, sizeof(path), "/proc/%s/stat", v[1]);
+
+ fp = fopen(path, "r"); // open stat file
  if(!fp) {
-    perror("Error opening status file");
-    return 1;
+   DIE("fopen stat"); // error opening file message
  }
- while(fgets(line,sizeof(line), fp)) {
-    if(strncmp(line,"PPid:", 5) == 0) {
-        printf("PPID: %s", line + 5);
-        break;
-    }
+ int pid, ppid; // parent pid
+ char comm[256],state; // command name and state
+ long utime,stime; // cpu times
+
+ // Read the fields needed from the stat file like pid, comm, state, ppid, utime, stime
+ if(fscanf(fp, "%d,%255s %c %d", &pid, comm, &state, &ppid) != 4) {
+   DIE_MSG("fscanf stat failed to read");
  }
- while(fgets(line,sizeof(line), fp)) {
-    if(strncmp(line,"VmRSS:", 6) == 0) {
-        printf("VmRSS: %s", line + 6);
-        break;
-    }
+
+ for (int i = 0; i<9; i++) fscanf(fp, "%*s"); // skip to utime
+ 
+ if (fscanf(fp, "%ld %ld", &utime, &stime) !=2){
+   DIE_MSG("failed to read CPU time");
  }
- fclose(fp);
+fclose(fp);
+
 }
