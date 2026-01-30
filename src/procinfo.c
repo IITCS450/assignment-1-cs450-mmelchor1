04e96553f2f41fb34979ab72d4df3b99dda9e405
mmelchor1 <mmelchor1@hawk.iit.edu>
2026-01-29 21:38:15 -0500

    printing part a

--- src/procinfo.c
+++ src/procinfo.c
@@ -21,7 +21,7 @@ int main(int c,char**v){
  long utime,stime; // cpu times
 
  // Read the fields needed from the stat file like pid, comm, state, ppid, utime, stime
- if(fscanf(fp, "%d,%255s %c %d", &pid, comm, &state, &ppid) != 4) {
+ if(fscanf(fp, "%d %255s %c %d", &pid, comm, &state, &ppid) != 4) {
    DIE_MSG("fscanf stat failed to read");
  }
 
@@ -30,6 +30,51 @@ int main(int c,char**v){
  if (fscanf(fp, "%ld %ld", &utime, &stime) !=2){
    DIE_MSG("failed to read CPU time");
  }
-fclose(fp);
+  fclose(fp);
 
+  // calculate cpu time in seconds
+  long ticks = sysconf(_SC_CLK_TCK);
+  double cpu_time = (double)(utime + stime)/ticks;
+
+  snprintf(path, sizeof(path), "/proc/%s/cmdline",v[1]); // path to cmdline file
+  fp = fopen(path, "r");
+  if(!fp){
+    DIE("fopen cmdline");
+  }
+  char cmdline[1024];
+  size_t n = fread(cmdline, 1, sizeof(cmdline)-1, fp); // read file 
+  fclose(fp);
+
+  if(n ==0)
+    strcpy(cmdline, "[empty]"); // handle empty cmdline
+  else {
+    for (size_t i=0; i<n-1; i++)
+      if(cmdline[i] == '\0') cmdline[i] =' '; // replace nulls with space
+    cmdline[n]='\0';
+  }
+
+  snprintf(path, sizeof(path), "/proc/%s/status", v[1]); // path to status file
+  fp = fopen(path, "r");
+  if(!fp){
+    DIE("fopen status");
+  }
+  char line[256];
+  char vmrss[64] = "VmRSS: N/A"; // default value
+
+  // read through file to find VmRSS
+  while(fgets(line, sizeof(line), fp)){
+    if(strncmp(line, "VmRSS:",6)==0){
+      sscanf(line,"VmRSS:%63[^\n]", vmrss);
+      break;
+    }
+  }
+  fclose(fp);
+
+  printf("PID: %d\n",pid); // print pid
+  printf("State: %c\n", state); // print state
+  printf("PPID: %d\n", ppid); // print parent pid
+  printf("Command Line: %s\n", cmdline); // print cmd line
+  printf("CPU Time: %.2f seconds\n", cpu_time); // print cpu time
+  printf("VmRSS:%s\n", vmrss); // print vmrss
+  return 0; 
 }
