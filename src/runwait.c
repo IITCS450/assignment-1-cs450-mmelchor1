14e08791cf944704ef4e2e79a631cc5bb4f42e22
mmelchor1 <mmelchor1@hawk.iit.edu>
2026-01-29 22:14:11 -0500

    part b completed passed test exit code

--- src/runwait.c
+++ src/runwait.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include "common.h"
 #include <sys/wait.h>
 #include <time.h>
@@ -6,7 +7,27 @@ static void usage(const char *a){fprintf(stderr,"Usage: %s <cmd> [args]\n",a); e
 static double d(struct timespec a, struct timespec b){
  return (b.tv_sec-a.tv_sec)+(b.tv_nsec-a.tv_nsec)/1e9;}
 int main(int c,char**v){
-/* TODO : ADD CODE HERE
-*/
+    if (c <2) usage(v[0]); // check for min arguments
+    struct timespec start,end;
+    clock_gettime(CLOCK_MONOTONIC, &start); // get start time
+    pid_t pid = fork();
+    if(pid <0) DIE("fork"); // fork error handling
+    if(pid ==0){ // child process
+        execvp(v[1],&v[1]);
+        DIE("execvp");
+    }
+    int status;
+    if(waitpid(pid,&status, 0)<0) // wait for child process
+        DIE("waitpid");
+    clock_gettime(CLOCK_MONOTONIC, &end); // get end time
+    printf("Child PID: %d\n", pid);
+    
+    if(WIFEXITED(status)){ // check if exited 
+        printf("exit=%d\n", WEXITSTATUS(status)); // print exit code
+    } else if (WIFSIGNALED(status)){
+        printf("Terminated by signal: %d\n", WTERMSIG(status)); // print signal
+    }
+    printf("elapsed:%.3f\n", d(start, end)); // print elapsed time
+
  return 0;
 }
